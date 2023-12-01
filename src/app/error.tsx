c2bd820233e1d7f73d3ed49e2462e3618a4d05d4
JhonSGIzquierdo <metalprime57@gmail.com>
2023-12-01 17:52:02 -0500

    Error Database Handle

---
+++ src/app/error.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useEffect } from 'react';
+ 
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Optionally log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+ 
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center">Database Not Connected!</h2>
+      <h3>Be sure everything is active before start the frontend</h3>
+    </main>
+  );
+}
